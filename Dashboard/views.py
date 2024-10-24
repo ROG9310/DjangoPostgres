edd5cb99aeed1d3abe7ffbdfd7c4077bf2b3c5c1
Cynthya Ramirez <cynthya.ramirez@alvamex.com.mx>
2024-10-24 08:34:35 -0600

    241024

--- Dashboard/views.py
+++ Dashboard/views.py
@@ -146,7 +146,7 @@ def empresa(request):
     
 @login_required   
 def cedis(request):
-    cedis = Ubicaciones.objects.filter(empresa == 'Alvamex')
+    cedis = Ubicaciones.objects.filter(empresa = 'Alvamex')
     return render(request,'cedis.html',{'cedis': cedis
     })
 
